ac7abfc0a1438d06b7f708f9408b61673a8b8eac
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-04-22 21:42:04 -0300

    conflitos

--- scripts/script.tela3.js
+++ scripts/script.tela3.js
@@ -8,17 +8,35 @@ function criarQuizz() {
         <h1>Comece pelo começo</h1>
         <div class="flex-center">
             <div class="inputs">
-                <input type="text" id="" placeholder="Título do seu quizz">
-                <input type="text" id="" placeholder="URL da imagem do seu quizz">
-                <input type="text" id="" placeholder="Quantidade de perguntas do quizz">
-                <input type="text" id="" placeholder="Quantidade de níveis do quizz">
+                <input type="text" id="titulo" placeholder="Título do seu quizz">
+                <input type="text" id="url-principal" placeholder="URL da imagem do seu quizz">
+                <input type="text" id="quantidade-de-perguntas" placeholder="Quantidade de perguntas do quizz">
+                <input type="text" id="quantidade-de-niveis" placeholder="Quantidade de níveis do quizz">
             </div>
         </div>
         <div class="flex-center">
-            <button onclick="criarPerguntas()" >Prosseguir pra criar perguntas</button>
+            <button onclick="validarInfos()">Prosseguir pra criar perguntas</button>
         </div>
     </main>
     `;
+}
+function validarInfos() {
+    const tituloQuizz = document.getElementById("titulo").value;
+    const urlPrincipal = document.getElementById("url-principal").value;
+    const numPerguntas = document.getElementById("quantidade-de-perguntas").value;
+    const numNiveis = document.getElementById("quantidade-de-niveis").value;
+    if (tituloQuizz.length >= 20 && tituloQuizz.length <= 65) {
+        if (urlPrincipal != "") {
+            console.log("ulr passou");
+            if (numPerguntas >= 3) {
+                console.log("num perguntas passou");
+                if (numNiveis >= 2) {
+                    console.log("num niveis passou");
+                    criarPerguntas();
+                } else { alert("Por favor preencha os dados corretamente. Seu quizz deve ter no mínimo 2 níveis") };
+            } else { alert("Por favor preencha os dados corretamente. Seu quizz deve ter no mínimo 3 perguntas") };
+        } else { alert("Por favor preencha os dados corretamente. Sua URL não é valida") };
+    } else { alert("Por favor preencha os dados corretamente. Seu titulo deve ter entre 20 e 65 caracteres") };
 }
 function criarPerguntas() {
     const tela3 = document.querySelector(".tela3");
