8feec2e0adfab591e3a1c9c401756131abd45b65
Adrian <adriancosta1215@gmail.com>
2022-04-22 21:15:27 -0300

    Imagem de quiz sem repetição e pequenas mudanças

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,32 @@
-window.onload = carregarPag()
+const conteudoPag = document.querySelector(".app");
 
+window.onload = carregarPag()
 
 cardQuiz = {
     id: 0,
     titulo: 0,
     imagem: 0,
 }
+const gradient = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%)`;
+let quizzesInfo, quizCard;
 
+function carregarPag() {
 
-const gradient = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%)`
-let quizzesInfo, quizCard
-const conteudoPag = document.querySelector(".body")
-
-function carregarPag(){
-    
-    const conteudoPag = document.querySelector(".body")
-    conteudoPag.innerHTML = ""
-    const pagInicial = `<header class="header">
-    <div class="topo"> <h1 class="logo" onclick="carregarPag()">BuzzQuizz</h1> </div>
-    </header>
+    conteudoPag.innerHTML = "";
+    const pagInicial = `
     <main class="pagina">
             <div class="delimitador">
 
                 <div class="quizzesCriados">
                     <p>Você não criou nenhum quizz ainda :(</p>
-                    <button class="criarQuizz" onclick = "telaCriarQuiz()">Criar Quizz</button>
+                    <button class="criarQuizz" onclick = "criarQuizz()">Criar Quizz</button>
                 </div>
 
                 <!-- Quando tiver quizzes criados pelo usuário já tem aqui o template pronto-->
                     
                 <div class="quizzesLista userQuizzes">
                     <div>
-                        <p>Seus Quizzes</p><ion-icon class="icon" name="add-circle" onclick = "telaCriarQuiz()"></ion-icon>
+                        <p>Seus Quizzes</p><ion-icon class="icon" name="add-circle" onclick = "criarQuizz()"></ion-icon>
                     </div>
                         <ul class="quizzes">
                                 
@@ -56,75 +51,61 @@ function carregarPag(){
                 </div>
             </div>
     </main>`
-    
+
     conteudoPag.innerHTML = pagInicial
-    
+
     exibirQuizzes()
-    
+
 }
 
 let topoImagemGrande
 
-function exibirQuizzes(){
+function exibirQuizzes() {
     axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes")
-    .then(function(resposta){
-        const lugarQuizzes = document.querySelector(".quizzes.todos")
-        quizzesInfo = resposta.data
-        console.log(lugarQuizzes)
-        lugarQuizzes.innerHTML = ""
+        .then(function (resposta) {
+            const lugarQuizzes = document.querySelector(".quizzes.todos")
+            quizzesInfo = resposta.data
+            console.log(lugarQuizzes)
+            lugarQuizzes.innerHTML = ""
+            for (let i = 0; i < quizzesInfo.length; i++) {
 
-        for(let i = 0; i < quizzesInfo.length; i++){
-            
-            lugarQuizzes.innerHTML += `<li class="quizz" onclick = "pegarID(this)">
-            <p class = "nomeQuiz">${quizzesInfo[i].title}</p>
-            <span class = "idQuiz hidden"> ${quizzesInfo[i].id} </span>
-            </li>`
+                lugarQuizzes.innerHTML += `
+                <li class="quizz" onclick = "pegarID(this)">
+                    <p class = "nomeQuiz">${quizzesInfo[i].title}</p>
+                    <span class = "idQuiz hidden"> ${quizzesInfo[i].id} </span>
+                </li>`
 
-            quizCard = document.querySelector(`.quizzes.todos li:nth-child(${i+1})`)
-
-            quizCard.style.setProperty("background-image", `${gradient}, url('${quizzesInfo[i].image}')`);
-            
-        }
-    })
+                quizCard = document.querySelector(`.quizzes.todos li:nth-child(${i + 1})`)
+                quizCard.style.setProperty("background-image", `${gradient}, url('${quizzesInfo[i].image}')`);
+                
+            }
+        })
 }
 
 
-function pegarID(quizClicado){
-    
-    
+function pegarID(quizClicado) {
+
+
     const idQuiz = quizClicado.querySelector(".idQuiz").innerHTML
-    for(let cont = 0; cont < quizzesInfo.length; cont++){
-    
-        if(idQuiz == quizzesInfo[cont].id){
-            
+    for (let cont = 0; cont < quizzesInfo.length; cont++) {
+
+        if (idQuiz == quizzesInfo[cont].id) {
+
             topoImagemGrande = quizzesInfo[cont].image
             cardQuiz.titulo = quizzesInfo[cont].title
         }
     }
-    
+
     mostrarQuiz()
 
 }
 
-function mostrarQuiz(){
-
+function mostrarQuiz() {
     conteudoPag.innerHTML = ""
-    conteudoPag.innerHTML = `<header class="header">
-    <div class="topo"> <h1 class="logo" onclick="carregarPag()">BuzzQuizz</h1> </div>
-    </header>`
+    conteudoPag.innerHTML = `<div class = "imagemQuiz"> </div>`
     const gradient2 = "linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57))"
-    const headImage = document.querySelector(".header")
-    headImage.innerHTML += `<div class = "imagemQuiz">
-    <p class = "quizTitle">${cardQuiz.titulo}</p></div>`
+    const headImage = document.querySelector(".imagemQuiz")
+    headImage.innerHTML += `
+    <p class = "quizTitle">${cardQuiz.titulo}</p>`
     headImage.style.setProperty("background-image", `${gradient2}, url('${topoImagemGrande}')`);
 }
-
-function telaCriarQuiz(){
-    conteudoPag.innerHTML = ""
-    conteudoPag.innerHTML = `<header class="header">
-    <div class="topo"> <h1 class="logo" onclick="carregarPag()">BuzzQuizz</h1> </div>
-    </header>
-    Aqui fica a pagina de criação do quiz :)`
-    
-}
-
